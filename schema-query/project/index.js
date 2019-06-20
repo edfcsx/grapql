9d6f23c24419ad32f8873ad6e5555819e2ff6de5
eduardo cipriano <edfcsx@gmail.com>
2019-06-20 07:03:10 -0300

    files for lesson 13

--- schema-query/project/index.js
+++ schema-query/project/index.js
@@ -1,1 +1,28 @@
-console.log('teste')
+const { ApolloServer, gql } = require('apollo-server')
+
+const typeDefs = gql`
+    scalar Date
+
+    type Query {
+        ola: String
+        newDate: Date
+    }
+`
+
+const resolvers = {
+    Query: {
+        ola() {
+            return 'Hello World'
+        },
+        newDate() {
+            return new Date().toString()
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server.listen().then(({ url }) => console.log(`Executando na url ${url}`))
